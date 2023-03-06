bed69de46bc618761ced3375171be1ecce7ed62b
Pheon-Dev <devpheon@gmail.com>
2023-03-06 16:27:23 +0300

    feat: next app for simulation

---
+++ apps/finance/src/app/page.tsx
@@ -0,0 +1,114 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { GetToken, C2BSimulation } from '../../../../packages/m-pesa'
+import styles from './page.module.css'
+
+const CONSUMER_SECRET = process.env.NEXT_PUBLIC_CONSUMER_SECRET;
+const CONSUMER_KEY = process.env.NEXT_PUBLIC_CONSUMER_KEY;
+const SHORT_CODE = process.env.NEXT_PUBLIC_SHORT_CODE;
+
+const inter = Inter({ subsets: ['latin'] })
+
+const token = GetToken({
+  consumerSecret: `${CONSUMER_SECRET}`,
+  consumerKey: `${CONSUMER_KEY}`,
+  environment: "development",
+});
+C2BSimulation({
+  consumerSecret: `${CONSUMER_SECRET}`,
+  consumerKey: `${CONSUMER_KEY}`,
+  environment: "development",
+}, {
+  shortCode: `${SHORT_CODE}`,
+  amount: "10",
+  msisdn: "254768858280",
+  commandID: "CustomerPayBillOnline",
+  billRefNumber: "000"
+});
+
+export default function Home() {
+  // console.log({ "token": token });
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+        <div className={styles.thirteen}>
+          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        </div>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={inter.className}>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p className={inter.className}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
